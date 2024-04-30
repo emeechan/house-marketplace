9c85647e02c26abbc96f4f05c06aaaac8051a322
Emily Meechan <emilymeechan1@gmail.com>
2024-04-30 15:40:13 +0100

    displaying and updating user details in the database

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,37 +1,55 @@
-import { useState, useEffect } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { useNavigate, Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 function Profile() {
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate();
-  const auth = getAuth();
+  const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(null);
-      }
-    });
+  const { name, email } = formData
 
-    return () => unsubscribe();
-  }, [auth]);
+  const navigate = useNavigate()
 
-  // Function to handle logout
   const onLogout = () => {
     auth.signOut()
-      .then(() => {
-        // Redirect to home page after logout
-        navigate('/');
-      })
-      .catch((error) => {
-        console.error('Logout error:', error);
-      });
-  };
+    navigate('/')
+  }
 
-  return user ? (
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // Update display name in fb
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        })
+
+        // Update in firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name,
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error('Could not update profile details')
+    }
+  }
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  return (
     <div className='profile'>
       <header className='profileHeader'>
         <p className='pageHeader'>My Profile</p>
@@ -39,13 +57,44 @@ function Profile() {
           Logout
         </button>
       </header>
-      {/* Display user information */}
-      <div className='userData'>
-        <p>Name: {user.displayName}</p>
-        <p>Email: {user.email}</p>
-      </div>
+
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p
+            className='changePersonalDetails'
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prevState) => !prevState)
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+
+        <div className='profileCard'>
+          <form>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type='text'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
-  ) : null;
+  )
 }
 
-export default Profile;
+export default Profile
